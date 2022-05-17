d4bc86f91301ebf5c5978a0745e5a2efa4355990
Ristellise <joshwoo71@gmail.com>
2022-05-17 21:55:29 +0800

    Remove Included AssCS, added gitmodule.

--- AssCS/AssInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Ameko.AssCS;
-
-/// <summary>
-/// Shortcut class for the ASS info dictionary
-/// </summary>
-/// <author>9volt</author>
-public class AssInfo : Dictionary<string, string>
-{
-}
--- AssCS/AssMeta.cs
+++
@@ -1,9 +0,0 @@
-namespace Ameko.AssCS;
-
-/// <summary>
-/// Shortcut class for the ASS Metadata dictionary
-/// </summary>
-/// <author>Zahuczky</author>
-public class AssMeta : Dictionary<string, string>
-{
-}
